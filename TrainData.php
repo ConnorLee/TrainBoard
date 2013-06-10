c34cd2f37dd216f01b5841f1108a734e8fba0a2c
Kristina Hu <kristina@betterment.com>
2013-06-10 17:08:04 -0400

    Created tables for 3 most recent dep/wd/g

--- TrainData.php
+++ TrainData.php
@@ -1,32 +1,53 @@
 <?php
 
 $xml = simplexml_load_file("dashboard.xml");
-$deposits = $xml->children()->deposits->children();
 
-echo "DEPOSIT INFORMATION";
-echo "";
-echo("<table>");
-$i = 0;
-foreach($deposits as $deposit)
-    {
-        echo("<tr>");
-        echo("<td>");
-        echo $deposit->fullName . "<br>";
-        echo("</td>");
-        echo("<td>");
-        echo $deposit->goalName . "<br>";
-        echo("</td>");
-        echo("<td>");
-        echo $deposit->amount . "<br>";
-        echo("</td>");
-        echo("<td>");
-        echo $deposit->description . "<br>";
-        echo("</td>");
-        echo("</tr>");
-        $i++;
-        if($i >= 3) 
-            break;
-    }
-echo("</table>");
+// Most Recent 3 Withdrawals
+$deposits = $xml->children()->deposits->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => $deposits[$i]->fullName,
+        "goalName" => $deposits[$i]->goalName,
+        "amount" => $deposits[$i]->amount,
+        "description" => $deposits[$i]->description,
+        );
+    $mostRecentThreeDeposits[$i]=$chart;
+}
+echo "<br>DEPOSIT INFORMATION <br>";
+print_r($mostRecentThreeDeposits);
+
+// Most Recent 3 Withdrawals
+$withdrawals = $xml->children()->withdrawals->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => $withdrawals[$i]->fullName,
+        "goalName" => $withdrawals[$i]->goalName,
+        "amount" => $withdrawals[$i]->amount,
+        "balance" => $withdrawals[$i]->balance,
+        "onOffTrack" => $withdrawals[$i]->onOffTrack,
+        );
+    $mostRecentThreeWithdrawals[$i]=$chart;
+}
+echo "<br>WITHDRAWAL INFORMATION<br>";
+print_r($mostRecentThreeWithdrawals);
+
+
+// Most Recent 3 Goals
+$goals = $xml->children()->goals->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => $goals[$i]->fullName,
+        "goalName" => $goals[$i]->goalName,
+        "goalAmount" => $goals[$i]->goalAmount,
+        "goalLength" => $goals[$i]->goalLength,
+        "initDeposit" => $goals[$i]->initDeposit,
+        );
+    $mostRecentThreeGoals[$i]=$chart;
+}
+echo "<br>GOAL INFORMATION <br>";
+print_r($mostRecentThreeGoals);
 
 ?>
