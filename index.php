1dc8accb07914e9c92d4cd72eabcb439cc94b46f
klesse413 <klesse413@gmail.com>
2013-06-14 17:45:31 -0400

    blah sorry more again

--- index.php
+++ index.php
@@ -8,59 +8,6 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">
 		</script>
 
-		<!-- Rotation Script-->
-		<script type="text/javascript">
-			$(document).ready(function(){
-				// set up hover panels
-				// although this can be done without JavaScript, we've attached these events
-				// because it causes the hover to be triggered when the element is tapped on a touch device
-
-				window.addEvents = function() {
-
-					$('.hover').hover(function(){
-						$(this).addClass('flip');
-					},function(){
-						$(this).removeClass('flip');
-					});
-
-					// set up click/tap panels
-					$('.click').toggle(function(){
-						$(this).addClass('flip');
-					},function(){
-						$(this).removeClass('flip');
-					});
-
-					// set up block configuration
-					$('.block .action').click(function(){
-						$('.block').addClass('flip');
-					});
-					$('.block .edit-submit').click(function(e){
-						$('.block').removeClass('flip');
-
-						// why not update that list for fun?
-						$('#list-title').text($('#form_title').val());
-						$('#list-items').empty();
-						for (var num = 1; num <= $('#form_items').val(); num++) {
-							$('#list-items').append('<li>Name '+num+'</li>');
-						}
-						e.preventDefault();
-					});
-
-					// set up contact form link
-					$('.contact .action').click(function(e){
-						$('.contact').addClass('flip');
-						e.preventDefault();
-					});
-					$('.contact .edit-submit').click(function(e){
-						$('.contact').removeClass('flip');
-						// just for effect we'll update the content
-						e.preventDefault();
-					});
-				}
-
-			});
-		</script>
-
 	</head>
 
 	<body>
@@ -856,13 +803,7 @@ for (var i in data.TransObject) {
 		 if(old != rowData[k]){
 			$pToUpdate.html(rowData[k]); // definitely works
 			// setTimeout(function(){
-				$pToUpdate.parent().parent().addClass('flip');
-			// }, (200*Number(i)));
-		// }
-
-		setTimeout(function(){
-			$pToUpdate.parent().parent().removeClass('flip');
-		}, 50);
+			$pToUpdate.parent().parent().toggleClass('flip');
 
 
 		}
