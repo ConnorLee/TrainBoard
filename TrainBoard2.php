63e7792eb83c9c20d48cbc3bd8eac740223a4cf6
connorlee <connorlee57@gmail.com>
2013-06-17 12:00:47 -0400

    added TrainBoard2

---
+++ TrainBoard2.php
@@ -0,0 +1,70 @@
+<?php
+
+$xml = simplexml_load_file("dashboard.xml");
+
+// Most Recent 3 Deposits
+$deposits = $xml->children()->deposits->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => (string)$deposits[$i]->fullName,
+        "description" => "Deposit",
+        "goalName" => (string)$deposits[$i]->goalName,
+        "goalAmount" => (string)$deposits[$i]->goalAmount,
+        "goalLength" => (string)$deposits[$i]->goalLength,
+        "amount" => (string)$deposits[$i]->amount,
+        "balance" => (string)$deposits[$i]->balance,
+        "onOffTrack" => (string)$deposits[$i]->onOffTrack,
+        );
+    $mostRecentThreeDeposits[$i]=$chart;
+}
+
+// Most Recent 3 Withdrawals
+$withdrawals = $xml->children()->withdrawals->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => (string)$withdrawals[$i]->fullName,
+        "description" => "Withdrawal",
+        "goalName" => (string)$withdrawals[$i]->goalName,
+        "goalAmount" => (string)$withdrawals[$i]->goalAmount,
+        "goalLength" => (string)$withdrawals[$i]->goalAmount,
+        "amount" => (string)$withdrawals[$i]->amount,
+        "balance" => (string)$withdrawals[$i]->balance,
+        "onOffTrack" => (string)$withdrawals[$i]->onOffTrack,
+        );
+    $mostRecentThreeWithdrawals[$i]=$chart;
+}
+
+// Most Recent 3 Goals
+$goals = $xml->children()->goals->children();
+for($i = 0; $i < 3; $i++)
+{
+    $chart = array(
+        "fullName" => (string)$goals[$i]->fullName,
+        "description" => "Goal",
+        "goalName" => (string)$goals[$i]->goalName,
+        "goalAmount" => (string)$goals[$i]->goalAmount,
+        "goalLength" => (string)$goals[$i]->goalLength,
+        "amount" => (string)$goals[$i]->initDeposit,
+        "balance" => NULL,
+        "onOffTrack" => NULL,
+        );
+    $mostRecentThreeGoals[$i]=$chart;
+}
+
+// Combine into one table
+
+$file = "TrainData.json";
+$fh = fopen($file, 'a') or die("can't open file");
+fwrite($fh, "DepositObject:");
+fwrite($fh, json_encode($mostRecentThreeDeposits));
+fwrite($fh, "]");
+fwrite($fh, "WithdrawalObject:");
+fwrite($fh, json_encode($mostRecentThreeWithdrawals));
+fwrite($fh, "]");
+fwrite($fh, "GoalObject:");
+fwrite($fh, json_encode($mostRecentThreeGoals));
+fwrite($fh, "]");
+fclose($fh);
+?>
